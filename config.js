74c04f2777d4f244319ff24d81a8799c4992a96d
NdizzXD270 <ndizzxd@gmail.com>
2025-09-12 06:36:56 +0700

    config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const config = {
+  token: '8241743968:AAFpEtitIk-lNvBLq400WzrrJfeaqcQWv7Q',
+  adminId: '8022912187', 
+  pp: 'https://files.catbox.moe/x2a4mf.jpg',
+urladmin: 'https://t.me/NdizzXD',
+    //SERVER 1
+  domain: 'https://privatexwarx.sistems.tech', // Isi dengan domain yang digunakan
+  plta: 'ptla_fJwc2JsxeKauH6cFOJFk1ez3Qgycr8VqnsP8qVjilfA', // Isi dengan nilai plta yang sesuai
+  pltc: 'ptlc_3iEzLSJQDTmkMlHGKzY7xXcmaMtbRyduObcJ0QKbeCJ', // Isi dengan nilai pltc yang sesuai
+  
+  //CREATE PANEL
+  loc: '1', // Isi dengan lokasi yang diinginkan
+  eggs: '15'
+};
+
+module.exports = config;
