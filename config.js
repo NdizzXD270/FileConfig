3e6f905ec352e6b203f4f099bec653f9d3af3903
NdizzXD270 <ndizzxd@gmail.com>
2025-09-14 11:16:02 +0700

    Update config.js via bot

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
 urladmin: 'https://t.me/NdizzXD',
     //SERVER 1
   domain: 'https://tristan.panelwota.my.id', // Isi dengan domain yang digunakan
-  plta: 'ptla_3TeZRw5lHBfGJi47Lv6DC4ZtMC9BIyMl2uqyNeoWcyJ', // Isi dengan nilai plta yang sesuai
+  plta: 'ptla_fkVSWq9WBuKFmY5Spjm87jTDAxGV1hQw12m43xLkh9D', // Isi dengan nilai plta yang sesuai
   pltc: 'ptlc_OPsO1vrxfZtlbMJTkLdp6KTxbNTDw93ZwQj2yt6493N', // Isi dengan nilai pltc yang sesuai
   
   //CREATE PANEL
