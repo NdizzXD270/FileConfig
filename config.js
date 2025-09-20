86158dd2b185218bb884fbd03d1e1d539ab43b99
NdizzXD270 <ndizzxd@gmail.com>
2025-09-20 09:55:55 +0700

    Update config.js via bot

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
   pp: 'https://files.catbox.moe/x2a4mf.jpg',
 urladmin: 'https://t.me/NdizzXD',
     //SERVER 1
-  domain: 'https://kenzzprivate.naell.my.id', // Isi dengan domain yang digunakan
+  domain: 'https://privatepublik.mysura.xyz', // Isi dengan domain yang digunakan
   plta: 'ptla_48r4ZaRCZxbs8oMosIJ9Ukz0uxAGQqEoKVb0IHE5Kmq', // Isi dengan nilai plta yang sesuai
   pltc: 'ptlc_6i0283olET8UIanYdJdUGVLTMpczVYzWWG3LaGB3nD3', // Isi dengan nilai pltc yang sesuai
   
