7a7cd5c812aaf0cf247d549fe4e18d0f7b98eb79
NdizzXD270 <ndizzxd@gmail.com>
2025-09-21 00:10:34 +0700

    Update config.js via bot

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
   pp: 'https://files.catbox.moe/x2a4mf.jpg',
 urladmin: 'https://t.me/NdizzXD',
     //SERVER 1
-  domain: 'https://privatepublik.mysura.xyz', // Isi dengan domain yang digunakan
+  domain: 'https://privatepanel.satoruuhost.tech', // Isi dengan domain yang digunakan
   plta: 'ptla_9aifWtFelv0Rdesmmy0hbkLEvLVU95I80xonZNOD4AE', // Isi dengan nilai plta yang sesuai
   pltc: 'ptlc_EEamByBmMv7bT5xiB1PJNaGgs3QRFTRt5e9pX6LFo4p', // Isi dengan nilai pltc yang sesuai
   
