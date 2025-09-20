bcf107cf4d8a566720efa117d90b517af1ee5924
NdizzXD270 <ndizzxd@gmail.com>
2025-09-20 09:58:11 +0700

    Update config.js via bot

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ urladmin: 'https://t.me/NdizzXD',
     //SERVER 1
   domain: 'https://privatepublik.mysura.xyz', // Isi dengan domain yang digunakan
   plta: 'ptla_9aifWtFelv0Rdesmmy0hbkLEvLVU95I80xonZNOD4AE', // Isi dengan nilai plta yang sesuai
-  pltc: 'ptlc_6i0283olET8UIanYdJdUGVLTMpczVYzWWG3LaGB3nD3', // Isi dengan nilai pltc yang sesuai
+  pltc: 'ptlc_EEamByBmMv7bT5xiB1PJNaGgs3QRFTRt5e9pX6LFo4p', // Isi dengan nilai pltc yang sesuai
   
   //CREATE PANEL
   loc: '1', // Isi dengan lokasi yang diinginkan
