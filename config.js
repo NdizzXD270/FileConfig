aeaa95e52369f2a29d67dfbcb6c98c8fe30b887a
NdizzXD270 <ndizzxd@gmail.com>
2025-09-27 19:11:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const config = {
   pp: 'https://files.catbox.moe/x2a4mf.jpg',
 urladmin: 'https://t.me/NdizzXD',
     //SERVER 1
-  domain: 'https://privatepanel.satoruuhost.tech', // Isi dengan domain yang digunakan
-  plta: 'ptla_ZbBjIsIPQtMsliDlP19YDRYFFCF2apDAi9dgofnriu8', // Isi dengan nilai plta yang sesuai
-  pltc: 'ptlc_rmy5ahgmgOblP2Yyo41WerG7FDGuHldgQF8eUTrah3u', // Isi dengan nilai pltc yang sesuai
+  domain: 'https://xwararea.publicserver.my.id', // Isi dengan domain yang digunakan
+  plta: 'ptla_2YR0nL8Xo3UlbzdCpyELnHBigXFfDJQ8vklmgfcBbsW', // Isi dengan nilai plta yang sesuai
+  pltc: 'ptlc_S8bQ5vTu15TVTo7PbjvTVVsgCbJdMKwYa9WiYJHOzcV', // Isi dengan nilai pltc yang sesuai
   
   //CREATE PANEL
   loc: '1', // Isi dengan lokasi yang diinginkan
