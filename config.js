e1bdc1c082b6d24748aa63f383ea4294e24b7dd3
NdizzXD270 <ndizzxd@gmail.com>
2025-10-02 19:19:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const config = {
   pp: 'https://files.catbox.moe/x2a4mf.jpg',
 urladmin: 'https://t.me/NdizzXD',
     //SERVER 1
-  domain: 'https://xwarrpanell.sistems.tech', // Isi dengan domain yang digunakan
-  plta: 'ptla_YwLe4zIePvBlsr90sDO1xdBzRcJOaAXYbVdGL31vQHP', // Isi dengan nilai plta yang sesuai
-  pltc: 'ptlc_HksAzavESnshXPGFWJuCjdruK0y6XmbJul7ukGddfnt', // Isi dengan nilai pltc yang sesuai
+  domain: 'https://vvipxwarr.sistems.tech', // Isi dengan domain yang digunakan
+  plta: 'ptla_oysEvXEGfTcto3tpf1FNJxgj4ciIWMflOY5UrPYbgR9', // Isi dengan nilai plta yang sesuai
+  pltc: 'ptlc_XHURQeOvTZClH3W5wjii2qV4E5aSqgVqgceRVBJXniz', // Isi dengan nilai pltc yang sesuai
   
   //CREATE PANEL
   loc: '1', // Isi dengan lokasi yang diinginkan
