c706dd7b79d8fab2ff2c95c9264bdb05ab76ea93
NdizzXD270 <ndizzxd@gmail.com>
2025-10-03 06:25:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
   pp: 'https://files.catbox.moe/x2a4mf.jpg',
 urladmin: 'https://t.me/NdizzXD',
     //SERVER 1
-  domain: 'https://ndizzxdprivate.sigmaboy.space/', // Isi dengan domain yang digunakan
+  domain: 'https://ndizzxdprivate.sigmaboy.space', // Isi dengan domain yang digunakan
   plta: 'ptla_gXouhzrkn9XhMX0EMQgLCGUsK6VnGuaRIfaoDyoHUu7', // Isi dengan nilai plta yang sesuai
   pltc: 'ptlc_4xE5qf9sq0Vleq1VP8IoKAsy2ADsO7IK8RqXprGRqOY', // Isi dengan nilai pltc yang sesuai
   
