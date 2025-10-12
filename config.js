a3120d2afc0cb3bab1871f87af33af2c74cfb64b
NdizzXD270 <ndizzxd@gmail.com>
2025-10-12 10:58:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const config = {
-  token: '8241743968:AAFpEtitIk-lNvBLq400WzrrJfeaqcQWv7Q',
+  token: '8402817522:AAF2QcU0Hk9pbDh-5m1sD_E-zIPQ3Llp_TA',
   adminId: '8022912187', 
   pp: 'https://files.catbox.moe/x2a4mf.jpg',
 urladmin: 'https://t.me/NdizzXD',
     //SERVER 1
   domain: 'https://ndizzxdprivateserver.spaced.my.id', // Isi dengan domain yang digunakan
-  plta: 'ptla_3XoDgsXvmQhQAiZM0qe8X9dRopGXnViwchn4yyde8Uq', // Isi dengan nilai plta yang sesuai
-  pltc: 'ptlc_ERtSotkALwUphZYE51SOSPhtXhkvVvj0xEMaZ5w4gd9', // Isi dengan nilai pltc yang sesuai
+  plta: 'ptla_IpY02X3T7Bh8xDq0IsWcptfFCORItut6rXkKq7DLhb9', // Isi dengan nilai plta yang sesuai
+  pltc: 'ptlc_sbw6NohrB27stkYTgYP2Ma0GB3smCLrUgsfz6JZKY2s', // Isi dengan nilai pltc yang sesuai
   
   //CREATE PANEL
   loc: '1', // Isi dengan lokasi yang diinginkan
